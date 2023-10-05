b4c5cd70d982789e41bb3c426f96f1a39f28261b
starbxy21 <128787117+starbxy21@users.noreply.github.com>
2023-10-05 11:46:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,62 +1,23 @@
-const carouselContainer = document.querySelector(".carousel-container");
-const carousel = document.querySelector(".carousel");
-const carList = document.querySelector(".car-list");
-const prevButton = document.querySelector(".prev");
-const nextButton = document.querySelector(".next");
+document.addEventListener("DOMContentLoaded", function() {
+    fetchCarPictures();
+});
 
-let currentIndex = 0;
-const carsData = [
-  { name: "Car 1", imageUrl: "car1.png" },
-  { name: "Car 2", imageUrl: "car2.png" },
-  { name: "Car 3", imageUrl: "car3.png" },
-  // Add more cars and image URLs here
-];
-
-function showCarousel() {
-  carousel.innerHTML = "";
-  carsData.forEach((car, index) => {
-    const slide = document.createElement("div");
-    slide.classList.add("slide");
-    if (index === currentIndex) {
-      slide.classList.add("active");
-    }
-    const img = document.createElement("img");
-    img.src = car.imageUrl;
-    img.alt = car.name;
-    slide.appendChild(img);
-    carousel.appendChild(slide);
-  });
-}
-
-function showCarList() {
-  carList.innerHTML = "";
-  carsData.forEach((car, index) => {
-    const carItem = document.createElement("div");
-    carItem.classList.add("car-item");
-    carItem.innerHTML = `
-      <img src="${car.imageUrl}" alt="${car.name}">
-      <h3>${car.name}</h3>
-    `;
-    carItem.addEventListener("click", () => {
-      currentIndex = index;
-      showCarousel();
+function fetchCarPictures() {
+    // Simulate fetching car pictures from the server
+    const carPictures = [
+        "car1.png",
+        "car2.png",
+        "car3.png",
+        // Add more image URLs here
+    ];
+    
+    const carGallery = document.querySelector("#car-gallery");
+    
+    carPictures.forEach(function(picture) {
+        const imgElement = document.createElement("img");
+        imgElement.src = "car-images/" + picture;
+        imgElement.alt = "Car Picture";
+        
+        carGallery.appendChild(imgElement);
     });
-    carList.appendChild(carItem);
-  });
 }
-
-function nextSlide() {
-  currentIndex = (currentIndex + 1) % carsData.length;
-  showCarousel();
-}
-
-function prevSlide() {
-  currentIndex = (currentIndex - 1 + carsData.length) % carsData.length;
-  showCarousel();
-}
-
-prevButton.addEventListener("click", prevSlide);
-nextButton.addEventListener("click", nextSlide);
-
-showCarousel();
-showCarList();
